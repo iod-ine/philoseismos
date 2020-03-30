b67d2e067070d4d1363747a7a17e947d4d6e71e5
Ivan Dubrovin <io.dubrovin@icloud.com>
2020-03-30 12:53:51 +0300

    add tests for general functions for getting file parameters

--- tests/test_segy/test_gfunc.py
+++ tests/test_segy/test_gfunc.py
@@ -13,3 +13,21 @@ def test_get_endiannes(manually_crafted_segy_file, manually_crafted_little_endia
 
     assert gfunc.get_endiannes(manually_crafted_segy_file) == '>'
     assert gfunc.get_endiannes(manually_crafted_little_endian_segy_file) == '<'
+
+
+def test_get_sample_format_code(manually_crafted_segy_file):
+    """ Test the general function for getting sample format code. """
+
+    assert gfunc.get_sample_format_code(manually_crafted_segy_file) == 3
+
+
+def test_get_sample_format(manually_crafted_segy_file):
+    """ Test the general function for getting sample format. """
+
+    assert gfunc.get_sample_format(manually_crafted_segy_file) == (2, 'h', '2-byte signed integer')
+
+
+def test_get_number_of_traces(manually_crafted_segy_file):
+    """ Test the general function for getting number of traces. """
+
+    assert gfunc.get_number_of_traces(manually_crafted_segy_file) == 24
