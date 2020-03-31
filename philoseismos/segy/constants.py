76795df798aacf793e607c528c21c4b51544d54f
Ivan Dubrovin <io.dubrovin@icloud.com>
2020-03-31 11:40:55 +0300

    add implementation of Geometry object

--- philoseismos/segy/constants.py
+++ philoseismos/segy/constants.py
@@ -67,3 +67,99 @@ SFC = {
     12: (8, 'Q', '8-byte, unsigned integer'),
     16: (1, 'B', '1-byte, unsigned integer')
 }
+
+# format string to unpack trace headers in one go. after these 232 bytes go 8 bytes of text
+THFS = 'iiiiiiihhhhiiiiiiiihhiiiihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhiiiiihhihhhhhhhhihh'
+
+# columns for Geometry DataFrame
+THCOLS = ['TRACENO',  # ordinal number of a trace
+          'fTRACENO',
+          'FFID',  # unique number of a shot
+          'CHAN',  # channel number
+          'SOURCE',  # number of a source point
+          'CDP',  # number of a common depth point
+          'SEQNO',  # ordinal number of a trace in the ensemble
+          'TRC_TYPE',  # ID code for a trace
+          'STACKCNT',  # number of vertically stacked traces
+          'TRFOLD',  # number of horizontally stacked traces
+          'Data Use',
+          'OFFSET',  # offset - distance between the source and the receiver
+          'REC_ELEV',  # receiver elevation
+          'SOU_ELEV',  # source elevation
+          'DEPTH',  # source depth from the surface
+          'REC_DATUM',  # elevation of the datum at the receiver
+          'SOU_DATUM',  # elevation of the datum at the source
+          'SOU_H2OD',  # water depth at the source
+          'REC_H2OD',  # water depth at the receiver
+          'ELEVSC',  # scalar to apply to the elevations
+          'COORDSC',  # scalar to apply to the coordinates
+          'SOU_X',
+          'SOU_Y',
+          'REC_X',
+          'REC_Y',
+          'Coordinate Units',
+          'Weathering Velocity',
+          'Subweathering Velocity',
+          'UPHOLE',  # vertical time at the source, ms
+          'REC_UPHOLE',  # vertical time at the receiver, ms
+          'SOU_STAT',  # a static correction at the source, ms
+          'REC_STAT',  # a static correction at the receiver, ms
+          'TOT_STAT',  # total static correction, ms
+          'Lag Time A',
+          'Lag Time B',
+          'Delay Recording Time',
+          'TLIVE_S',  # starting time of muting, ms
+          'TFULL_S',  # ending time of muting, ms
+          'NUMSMP',  # number of samples in a trace (RadExPro needs this!)
+          'DT',  # sample interval in microseconds (RadExPro needs this!)
+          'IGAIN',  # code of the gain type of the instrument
+          'PREAMP',  # amplification coeefitient for instrument, dB
+          'EARLYG',  # initial amplification of the instrument, dB
+          'COR_FLAG',  # correlation flag (1 - no, 2 - yes)
+          'SWEEPFREQSTART',  # starting frequency of the sweep, Hz
+          'SWEEPFREQEND',  # ending frequency of the sweep, Hz
+          'SWEEPLEN',  # length of the sweep, ms
+          'SWEEPTYPE',  # code for the sweep type
+          'Sweep Taper (start)',
+          'Sweep Taper (end)',
+          'Taper Type',
+          'AAXFILT',  # frequency of the anti-aliasing filter, Hz
+          'AAXSLOP',  # slope of the anti-aliasing filter, dB / oct
+          'FREQXN',  # frequency of the reject filter, Hz
+          'FXNSLOP',  # slope of the reject filter, dB / oct
+          'FREQXL',  # low-cut frequency, Hz
+          'FREQXH',  # high-cut frequency, Hz
+          'FXLSLOP',  # low-cut slope, dB / oct
+          'FXHSLOP',  # high-cut slope, dB / oct
+          'YEAR',
+          'DAY',
+          'HOUR',
+          'MINUTE',
+          'SECOND',
+          'Time Basis',
+          'Weighting Factor',
+          'Group No. of Roll Switch',
+          'Group No. of First Trace',
+          'Group No. of Last Trace',
+          'Gap Size',
+          'Over Travel',
+          'CDP_X',
+          'CDP_Y',
+          'In-line No.',
+          'Cross-line No.',
+          'Shotpoint Number',
+          'Shotpoint Scalar',
+          'Trace Measurement Unit',
+          'Transduction Constant (mantissa)',
+          'Transduction Constant (10 power exponent)',
+          'Transduction Units',
+          'Device/Trace ID',
+          'Times Scalar',
+          'Source Type/Orientation',
+          'Source Energy Direction (vertical)',
+          'Source Energy Direction (cross-line)',
+          'Source Energy Direction (in-line)',
+          'Source Measurement (mantissa)',
+          'Source Measurement (10 power exponent)',
+          'Source Measurement Unit']
+# then go 8 bytes of text - so called "Header name"
---
+++ philoseismos/segy/g.py
@@ -0,0 +1,159 @@
+""" philoseismos: engineering seismologist's toolbox.
+
+author: Ivan Dubrovin
+e-mail: io.dubrovin@icloud.com """
+
+import struct
+import numpy as np
+import pandas as pd
+
+from philoseismos.segy import gfunc
+from philoseismos.segy import constants as const
+
+
+class Geometry:
+    """ This object represents trace headers of a SEG-Y file. """
+
+    def __init__(self):
+        self._df = None
+
+    @classmethod
+    def load(cls, file: str):
+        """ Load the Geometry from a SEG-Y file. """
+
+        g = cls()
+
+        with open(file, 'br') as sgy:
+            # grab endian, number of traces, sample size, and trace length
+            endian = gfunc.grab_endiannes(sgy)
+            nt = gfunc.grab_number_of_traces(sgy)
+            ss = gfunc.grab_sample_format(sgy)[0]
+            tl = gfunc.grab_trace_length(sgy)
+
+            # skip TFH and BFH
+            sgy.seek(3600)
+
+            # create a matrix to store trace headers' values
+            print("ATTENTION", nt)
+            data = np.empty(shape=(nt, 90), dtype=np.int32)
+
+            for i in range(nt):
+                # read in, unpack and store the header
+                raw_header = sgy.read(240)
+                header = struct.unpack(endian + const.THFS, raw_header[:232])
+                data[i] = header
+
+                # skip to the next header - one trace worth of bytes
+                sgy.seek(sgy.tell() + ss * tl)
+
+        # transform the matrix into a DataFrame
+        g._df = pd.DataFrame(data, index=range(nt), columns=const.THCOLS)
+
+        # apply scalars to elevations and coordinates
+        g._apply_scalars_after_unpacking()
+
+        return g
+
+    @property
+    def loc(self):
+        return self._df.loc
+
+    def _apply_scalars_after_unpacking(self):
+        """ Apply elevation and coordinate scalars after unpacking. """
+
+        # zero should be treated as one
+        self._df['ELEVSC'].loc[self._df['ELEVSC'] == 0] = 1
+        self._df['COORDSC'].loc[self._df['COORDSC'] == 0] = 1
+
+        # take the absolute value of the scalars
+        abs_elevsc = abs(self._df.ELEVSC)
+        abs_coordsc = abs(self._df.COORDSC)
+
+        # if negative, to be used as a divisor
+        neg_elevsc_ind = self._df.ELEVSC < 0
+        neg_coordsc_ind = self._df.COORDSC < 0
+
+        self._df.loc[neg_elevsc_ind, 'REC_ELEV'] /= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'SOU_ELEV'] /= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'DEPTH'] /= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'REC_DATUM'] /= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'SOU_DATUM'] /= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'SOU_H2OD'] /= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'REC_H2OD'] /= abs_elevsc
+
+        self._df.loc[neg_coordsc_ind, 'SOU_X'] /= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'SOU_Y'] /= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'REC_X'] /= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'REC_Y'] /= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'CDP_X'] /= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'CDP_Y'] /= abs_coordsc
+
+        # if positive, to be used as a multiplier
+        pos_elevsc_ind = self._df.ELEVSC > 0
+        pos_coordsc_ind = self._df.COORDSC > 0
+
+        self._df.loc[pos_elevsc_ind, 'REC_ELEV'] *= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'SOU_ELEV'] *= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'DEPTH'] *= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'REC_DATUM'] *= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'SOU_DATUM'] *= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'SOU_H2OD'] *= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'REC_H2OD'] *= abs_elevsc
+
+        self._df.loc[pos_coordsc_ind, 'SOU_X'] *= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'SOU_Y'] *= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'REC_X'] *= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'REC_Y'] *= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'CDP_X'] *= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'CDP_Y'] *= abs_coordsc
+
+    def _apply_scalars_before_packing(self):
+        """ Apply elevation and coordinate scalars before packing. """
+
+        # zero should be treated as one
+        self._df['ELEVSC'].loc[self._df['ELEVSC'] == 0] = 1
+        self._df['COORDSC'].loc[self._df['COORDSC'] == 0] = 1
+
+        # take the absolute value of the scalars
+        abs_elevsc = abs(self._df.ELEVSC)
+        abs_coordsc = abs(self._df.COORDSC)
+
+        # for unpacking: if negative, to be used as a divisor
+        # so, multiply before packing
+        neg_elevsc_ind = self._df.ELEVSC < 0
+        neg_coordsc_ind = self._df.COORDSC < 0
+
+        self._df.loc[neg_elevsc_ind, 'REC_ELEV'] *= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'SOU_ELEV'] *= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'DEPTH'] *= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'REC_DATUM'] *= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'SOU_DATUM'] *= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'SOU_H2OD'] *= abs_elevsc
+        self._df.loc[neg_elevsc_ind, 'REC_H2OD'] *= abs_elevsc
+
+        self._df.loc[neg_coordsc_ind, 'SOU_X'] *= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'SOU_Y'] *= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'REC_X'] *= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'REC_Y'] *= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'CDP_X'] *= abs_coordsc
+        self._df.loc[neg_coordsc_ind, 'CDP_Y'] *= abs_coordsc
+
+        # for unpacking: if positive, to be used as a multiplier
+        # so, divide before packing
+        pos_elevsc_ind = self._df.ELEVSC > 0
+        pos_coordsc_ind = self._df.COORDSC > 0
+
+        self._df.loc[pos_elevsc_ind, 'REC_ELEV'] /= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'SOU_ELEV'] /= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'DEPTH'] /= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'REC_DATUM'] /= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'SOU_DATUM'] /= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'SOU_H2OD'] /= abs_elevsc
+        self._df.loc[pos_elevsc_ind, 'REC_H2OD'] /= abs_elevsc
+
+        self._df.loc[pos_coordsc_ind, 'SOU_X'] /= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'SOU_Y'] /= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'REC_X'] /= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'REC_Y'] /= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'CDP_X'] /= abs_coordsc
+        self._df.loc[pos_coordsc_ind, 'CDP_Y'] /= abs_coordsc
