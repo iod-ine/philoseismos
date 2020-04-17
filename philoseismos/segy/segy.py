a3b33df90b971fdc460702af78e1f95152713da8
Ivan Dubrovin <io.dubrovin@icloud.com>
2020-04-17 11:22:07 +0300

    fix another typo in SegY.from_data_matrix

--- philoseismos/segy/segy.py
+++ philoseismos/segy/segy.py
@@ -185,6 +185,6 @@ class SegY:
         segy.bfh['byte_offset_of_data'] = 3600
         segy.bfh['no_traces'] = dm._m.shape[0]
 
-        segy.g = dm._headers
+        segy.g._df = dm._headers
 
         return segy
