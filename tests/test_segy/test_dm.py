3c46d26c5cd5df18e020d7105dd092d75831f113
Ivan Dubrovin <io.dubrovin@icloud.com>
2020-04-27 13:38:34 +0300

    Add tests for resample and filter methods

--- tests/test_segy/test_dm.py
+++ tests/test_segy/test_dm.py
@@ -5,9 +5,12 @@ This file contains tests for DataMatrix object.
 author: Ivan Dubrovin
 e-mail: io.dubrovin@icloud.com """
 
+import pytest
 import numpy as np
 
 from philoseismos.segy.dm import DataMatrix
+from philoseismos.segy.segy import SegY
+from philoseismos.segy.g import Geometry
 
 
 def test_loading_from_file(manually_crafted_segy_file):
@@ -23,3 +26,48 @@ def test_loading_from_file(manually_crafted_segy_file):
     # check that time axis is constructed correctly
     assert dm.dt == 500
     assert np.alltrue(dm.t == np.arange(0, 256, 0.5))
+
+
+def test_resample(manually_crafted_segy_file):
+    """ Test the resample method of the DataMatrix. """
+
+    segy = SegY.load(manually_crafted_segy_file)
+    dm = segy.dm
+
+    assert repr(dm) == 'DataMatrix: 24 traces, 512 samples, dt=500'
+    assert dm._m.shape == (24, 512)
+    assert np.alltrue(dm.t == np.arange(0, 256, 0.5))
+
+    new = dm.resample(1000)
+    assert repr(new) == 'DataMatrix: 24 traces, 256 samples, dt=1000'
+    assert new._m.shape == (24, 256)
+    assert np.alltrue(new.t == np.arange(0, 256, 1))
+
+    new = dm.resample(2000)
+    assert repr(new) == 'DataMatrix: 24 traces, 128 samples, dt=2000'
+    assert new._m.shape == (24, 128)
+    assert np.alltrue(new.t == np.arange(0, 256, 2))
+
+    with pytest.raises(ValueError):
+        new = dm.resample(256)
+
+
+def test_filter(manually_crafted_segy_file):
+    """ Test the filter method of the DataMatrix. """
+
+    segy = SegY.load(manually_crafted_segy_file)
+    dm = segy.dm
+
+    assert isinstance(dm._headers, Geometry)
+    assert dm._m.shape == (24, 512)
+    assert np.alltrue(dm._headers.loc[:, 'REC_X'] == np.arange(0, 48, 2))
+
+    new = dm.filter('REC_X', 10, 40, 2)
+    assert isinstance(new._headers, Geometry)
+    assert new._m.shape == (16, 512)
+    assert np.alltrue(new._headers.loc[:, 'REC_X'] == np.arange(10, 42, 2))
+
+    new = dm.filter('REC_X', 0, 24, 4)
+    assert isinstance(new._headers, Geometry)
+    # assert new._m.shape == (7, 512)
+    assert np.alltrue(new._headers.loc[:, 'REC_X'] == np.arange(0, 28, 4))
