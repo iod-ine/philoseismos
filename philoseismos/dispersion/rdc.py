5aaae30f55c79045bfbe331acc3727cfa2cda1e2
Ivan Dubrovin <io.dubrovin@icloud.com>
2020-05-04 10:25:58 +0300

    Add method to load rdcscalc results to RDC

--- philoseismos/dispersion/rdc.py
+++ philoseismos/dispersion/rdc.py
@@ -4,6 +4,7 @@ author: Ivan Dubrovin
 e-mail: io.dubrovin@icloud.com """
 
 import numpy as np
+import pandas as pd
 
 
 class RayleighDispersionCurve:
@@ -16,9 +17,18 @@ class RayleighDispersionCurve:
         self.modal_curves = []
 
     def load(self, file):
-        """ Load the DCs generated with rdcscalc proogram. """
+        """ Load the DCs generated with rdcscalc program. """
 
-        pass
+        curves = pd.read_csv(file)
+
+        self.modal_curves = [curves['Fundamental']]
+        self.modal_curves.append(curves['1st'])
+        self.modal_curves.append(curves['2nd'])
+        self.modal_curves.append(curves['3rd'])
+        self.modal_curves.append(curves['4th'])
+        self.modal_curves.append(curves['5th'])
+
+        self.freqs = np.arange(0, 500, 1) * 0.1 + 0.1
 
     def export_modal_curves_for_radex(self, filename, *, sou_x=500, rec_x_0, rec_x_1):
         """ Export modal curves to a text file accepted by MASW module in RadExPro. """
